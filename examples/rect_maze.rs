3cd740e03f7d64b82babb1dc9a63b0725d0269f1
Alexey Voznyuk <me@swizard.info>
2018-04-29 02:05:09 +0300

    port `rect_maze` example to `vec_slist::RandomTree`

--- examples/rect_maze.rs
+++ examples/rect_maze.rs
@@ -1,6 +1,7 @@
 extern crate rtt;
 extern crate rand;
 
+use rtt::util::vec_slist::{self, TransChecker, Expander};
 use rand::Rng;
 
 type Map<'a> = &'a [&'a [u8]];
@@ -30,10 +31,9 @@ fn main() {
     let mut rng = rand::thread_rng();
     let mut iters = 0;
 
-    let rtt = RandomTree::new(maze);
     let outcome = rtt::plan(
         // RandomTree
-        rtt,
+        vec_slist::RandomTree::new(ContextManager(maze)),
         // Sampler (pick a random cell on a map)
         |_: &_| {
             let row = rng.gen_range(0, height);
@@ -46,14 +46,15 @@ fn main() {
             Ok::<_, ()>(iters > 10000)
         },
         // GoalChecker (check if finish is reached)
-        |node: &RandomTreeNode<'_>| Ok::<_, ()>(node.coord() == finish),
+        |node: &vec_slist::RandomTreeNode<_, _>| Ok::<_, ()>(node.state() == &finish),
         // init state (start position in a maze)
         start,
     ).unwrap();
 
     println!("Map with {} rows and {} columns, start = {:?}, finish = {:?}", width, height, start, finish);
     match outcome {
-        rtt::Outcome::PathPlanned(path) => {
+        rtt::Outcome::PathPlanned(rev_path) => {
+            let path: Vec<_> = rev_path.collect();
             println!("Path planned:");
             for item in cells_iter(maze) {
                 match item {
@@ -74,135 +75,79 @@ fn main() {
 type Coord = (usize, usize);
 type State = Coord;
 
-struct PathNode {
-    coord: Coord,
-    prev: Option<usize>,
+#[derive(PartialEq, PartialOrd)]
+enum Dist {
+    Straight(usize),
+    NotStraight,
 }
 
-struct Tree<'a> {
-    nodes: Vec<PathNode>,
-    map: Map<'a>,
-}
+struct ContextManager<'a>(Map<'a>);
 
-struct RandomTree<'a>(Tree<'a>);
-
-impl<'a> RandomTree<'a> {
-    fn new(map: Map<'a>) -> RandomTree<'a> {
-        RandomTree(Tree { nodes: Vec::new(), map, })
-    }
-}
-
-impl<'a> rtt::RandomTree for RandomTree<'a> {
+impl<'a> vec_slist::ContextManager for ContextManager<'a> {
     type State = State;
     type Error = ();
-    type Node = RandomTreeNode<'a>;
+    type Dist = Dist;
 
-    fn make_root(mut self, state: Self::State) -> Result<Self::Node, Self::Error> {
-        self.0.nodes.clear();
-        self.0.nodes.push(PathNode { coord: state, prev: None, });
-        Ok(RandomTreeNode {
-            tree: self.0,
-            node: 0,
+    fn metric_distance(&mut self, node: &Self::State, probe: &Self::State) ->
+        Result<Self::Dist, Self::Error>
+    {
+        Ok(if let Some(path_iter) = StraightPathIter::new(node, probe) {
+            Dist::Straight(path_iter.count())
+        } else {
+            Dist::NotStraight
         })
     }
 
-    fn nearest_node(self, state: &Self::State) -> Result<Self::Node, Self::Error> {
-        let nearest = self.0.nodes
-            .iter()
-            .enumerate()
-            .flat_map(|(index, pn)| {
-                StraightPathIter::new(pn.coord, *state)
-                    .map(|it| (it.count(), index))
-            })
-            .min_by_key(|v| v.0);
-
-        Ok(RandomTreeNode {
-            tree: self.0,
-            node: nearest.map(|m| m.1).unwrap_or(0),
-        })
-    }
-}
-
-struct RandomTreeNode<'a> {
-    tree: Tree<'a>,
-    node: usize,
-}
-
-impl<'a> RandomTreeNode<'a> {
-    fn coord(&self) -> Coord {
-        self.tree.nodes[self.node].coord
-    }
-}
-
-impl<'a> rtt::RandomTreeNode for RandomTreeNode<'a> {
-    type State = State;
-    type Error = ();
-    type Tree = RandomTree<'a>;
-    type Path = Vec<Coord>;
-
-    fn expand(mut self, state: Self::State) -> Result<Self, Self::Error> {
-        let mut node_index = self.node;
-        if let Some(path_iter) = StraightPathIter::new(self.tree.nodes[node_index].coord, state) {
+    fn generate_trans<T>(&self, probe: Self::State, node_trans: T) ->
+        Result<Option<Self::State>, Self::Error>
+        where T: TransChecker<Self::State, Self::Error>
+    {
+        let start_coord = *node_trans.current();
+        if let Some(path_iter) = StraightPathIter::new(&start_coord, &probe) {
             for coord in path_iter {
-                let next_index = self.tree.nodes.len();
-                self.tree.nodes.push(PathNode { coord, prev: Some(node_index), });
-                node_index = next_index;
-            }
-        }
-        Ok(RandomTreeNode { tree: self.tree, node: node_index, })
-    }
-
-    fn transition(&self, random_state: Self::State) -> Result<Option<Self::State>, Self::Error> {
-        if let Some(path_iter) = StraightPathIter::new(self.coord(), random_state) {
-            for coord in path_iter {
-                if self.tree.map[coord.0][coord.1] == b'#' {
+                if self.0[coord.0][coord.1] == b'#' {
                     return Ok(None);
                 }
-                if self.tree.nodes.iter().any(|n| n.coord == coord) {
+                if node_trans.already_visited(&coord, |a, b| Ok(a == b))? {
                     return Ok(None);
                 }
             }
-            Ok(Some(random_state))
+            Ok(Some(probe))
         } else {
             Ok(None)
         }
     }
 
-    fn into_tree(self) -> Self::Tree {
-        RandomTree(self.tree)
-    }
-
-    fn into_path(self) -> Self::Path {
-        let mut path = Vec::new();
-        let mut maybe_index = Some(self.node);
-        while let Some(node_index) = maybe_index {
-            let node = &self.tree.nodes[node_index];
-            path.push(node.coord);
-            maybe_index = node.prev;
+    fn generate_expand<E>(&mut self, probe: Self::State, node_expander: E) ->
+        Result<(), Self::Error>
+        where E: Expander<Self::State, Self::Error>
+    {
+        let coord = *node_expander.current();
+        if let Some(path_iter) = StraightPathIter::new(&coord, &probe) {
+            node_expander.expand(path_iter.map(Ok))?;
         }
-        path.reverse();
-        path
+        Ok(())
     }
 }
 
-struct StraightPathIter {
+struct StraightPathIter<'a> {
     source: Coord,
-    target: Coord,
+    target: &'a Coord,
 }
 
-impl StraightPathIter {
-    fn new(source: Coord, target: Coord) -> Option<StraightPathIter> {
+impl<'a> StraightPathIter<'a> {
+    fn new(source: &'a Coord, target: &'a Coord) -> Option<StraightPathIter<'a>> {
         if source == target {
             None
         } else if source.0 == target.0 || source.1 == target.1 {
-            Some(StraightPathIter { source, target, })
+            Some(StraightPathIter { source: *source, target, })
         } else {
             None
         }
     }
 }
 
-impl Iterator for StraightPathIter {
+impl<'a> Iterator for StraightPathIter<'a> {
     type Item = Coord;
 
     fn next(&mut self) -> Option<Self::Item> {
